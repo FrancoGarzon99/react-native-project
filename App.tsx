ccc19ca10f4640b7e024a6fc2a648c9efb06c551
Franco.Dev <Franco.dev99@gmail.com>
2021-08-05 10:08:53 -0300

    Feat(package.json) install navigation react native

--- App.tsx
+++ App.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native';
 
-interface Props {
 
-}
-
-const App = (props: Props) => {
+const App = () => {
   return (
     <View>
+
       <StatusBar style="auto" />
       <Text>APP TSX</Text>
     </View>
