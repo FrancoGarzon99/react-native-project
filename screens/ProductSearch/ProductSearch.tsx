62e0a615a441b6e1482186933123a32f419e1f8b
Franco.Dev <Franco.dev99@gmail.com>
2021-09-02 01:43:07 -0300

    Feat(ProductSearch) add search product from firebase query

--- screens/ProductSearch/ProductSearch.tsx
+++ screens/ProductSearch/ProductSearch.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
-import { Button, Text, View, ScrollView, StyleSheet } from 'react-native';
-import Firebase from '../../DataBase/Firebase';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Firebase from '../../dataBase/Firebase';
+import SearchProduct from './SearchProduct/SearchProduct';
+import { Box, Text, View, ScrollView, Spinner } from 'native-base';
 
 interface Props {
 
@@ -8,42 +10,55 @@ interface Props {
 
 const ProductSearch = (props: Props) => {
 
-  const [dataSearch, setDataSearch] = React.useState({});
+  const [ShowHideScanner, setShowHideScanner] = useState<boolean>(false);
+  const [dataStringScanner, setDataStringScanner] = useState<{ type: string, data: string; } | undefined>(undefined);
+  const [productSearchRequest, setProductSearchRequest] = useState<Array<any>>([]);
 
   useEffect(() => {
-
     FirebaseData();
-  }, []);
+  }, [dataStringScanner !== undefined]);
 
   const FirebaseData = async () => {
-    const dataDocument = await Firebase.db.collection('products')
-      .where("code", "==", "7909189091713")
-      .get();
-
-    dataDocument.forEach(querySnapshot => console.log(querySnapshot.data()));
-
+    return await Firebase.db.collection('products')
+      .where("code", "==", `${dataStringScanner?.data}`)
+      .get().then(querySnapshot => {
+        if (querySnapshot.empty) {
+          console.log('El Producto que buscas no existe!');
+        } else {
+          querySnapshot.forEach(documentSnapshot => setProductSearchRequest([...productSearchRequest, documentSnapshot.data()]));
+        }
+      });
   };
-
+  console.log(productSearchRequest, 'Productos');
   return (
-    <View style={styleProduct.containerProductSearch}>
-      <View style={styleProduct.buttonsProductSearch}>
-        <Button title="Buscar un Producto" onPress={() => console.log("buscar producto")} />
+    <View>
+      <View>
+        <Button title="Buscar Producto" onPress={() => {
+          setShowHideScanner(!ShowHideScanner);
+          setDataStringScanner(undefined);
+        }} />
         <Button title="Crear lista de Productos" onPress={() => console.log("buscar producto")} />
       </View>
+      <View>
+        <SearchProduct ShowHideScanner={ShowHideScanner} setShowHideScanner={setShowHideScanner} setDataSearch={setDataStringScanner} />
+      </View>
       <ScrollView>
+        {productSearchRequest.length > 0 ? productSearchRequest.map(item => (
+          <Box key={item.code} bg="white" shadow={3} rounded="lg" maxWidth="90%">
+            <Text>{item.brand}</Text>
+            <Text>{item.name}</Text>
+            <Text>{item.code}</Text>
+            <Text>{item.price}</Text>
+          </Box>
+        )) :
+          <Spinner color="blue.500" />
+        }
       </ScrollView>
+
     </View>
   );
 };
 const styleProduct = StyleSheet.create({
-  containerProductSearch: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
 
-  },
-  buttonsProductSearch: {
-
-  }
 });
 export default ProductSearch;
